af6169c2dd67e27c05e60706f81878ad5131eb07
Muhammed Yaseen Poonawala <muhammed.yaseen110@gmail.com>
2019-03-27 17:37:03 +0500

    static changes

--- urls.py
+++ urls.py
@@ -9,6 +9,8 @@ from django.contrib import admin
 from django.contrib import auth
 from django.shortcuts import redirect
 from django.urls.base import reverse
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 def home(request):
@@ -25,6 +27,5 @@ urlpatterns = [
     # url('', admin.site.urls),
     # url('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG: 
-	urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
